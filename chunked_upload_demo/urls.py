6d02a3892b72712876792226c0d309f55f2a74dc
Jerin Peter George <jerinpetergeorge@gmail.com>
2019-12-01 18:43:01 +0530

    new version of url configuration added

--- chunked_upload_demo/urls.py
+++ chunked_upload_demo/urls.py
@@ -1,21 +1,17 @@
 from django.conf import settings
-from django.conf.urls import patterns, url
-
+from django.conf.urls import url
+from django.conf.urls.static import static
 from demo.views import (
     ChunkedUploadDemo, MyChunkedUploadView, MyChunkedUploadCompleteView
 )
 
+urlpatterns = [
 
-urlpatterns = patterns(
-    '',
-    url(r'^/?$',
-        ChunkedUploadDemo.as_view(), name='chunked_upload'),
-    url(r'^api/chunked_upload/?$',
-        MyChunkedUploadView.as_view(), name='api_chunked_upload'),
-    url(r'^api/chunked_upload_complete/?$',
-        MyChunkedUploadCompleteView.as_view(),
-        name='api_chunked_upload_complete'),
-    url(r'^static/(.*)$',
-        'django.views.static.serve',
-        {'document_root': settings.STATIC_ROOT, 'show_indexes': False}),
-)
+                  url(r'^$',
+                      ChunkedUploadDemo.as_view(), name='chunked_upload'),
+                  url(r'^api/chunked_upload/?$',
+                      MyChunkedUploadView.as_view(), name='api_chunked_upload'),
+                  url(r'^api/chunked_upload_complete/?$',
+                      MyChunkedUploadCompleteView.as_view(),
+                      name='api_chunked_upload_complete'),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
